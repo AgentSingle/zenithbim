ba79656d6ff7f8f67bba5f8b111e8d923309dfe6
AgentSingle <agentsingle404@gmail.com>
2026-02-24 12:01:49 +0530

    Updates

---
+++ assets/Service-CwBm8Kdd.js
@@ -0,0 +1,1 @@
+const c={__name:"Service",setup(e){return(r,_)=>" service "}};export{c as default};
---
+++ assets/serviceDetails-BYYrfVPV.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-BbiGpb0u.js";const r={};function c(t,s){return" Service Details "}const n=e(r,[["render",c]]);export{n as default};
